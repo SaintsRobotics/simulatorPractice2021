6997c1042d54abe4bb193af54ba36ac74001344c
noam987 <50681033+noam987@users.noreply.github.com>
2021-02-26 18:13:41 -0800

    added simulation support for rotation encoders (#5)
    * outputted voltage
    
    * wip absolute encoder simulator docs
    
    * almost done w docs.  just need frame of ref
    
    just need frame of reference,
    which way to turn the encoder will increase the voltage?
    
    * Fixed the issue with the pid By adding inversion
    
    * finished docs on AbsoluteEncoder
    **just need to how offsets are used in calculatoin
    
    * Fixed offset bug
    
    Co-authored-by: TiaSinghania <sonicstormwind@gmail.com>
    Co-authored-by: delaynieym <delaynieym@outlook.com>

--- src/main/java/frc/robot/AbsoluteEncoder.java
+++ src/main/java/frc/robot/AbsoluteEncoder.java
@@ -8,32 +8,58 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.simulation.AnalogInputSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Add your docs here.
  */
 public class AbsoluteEncoder {
     private AnalogInput analogIn;
+    private AnalogInputSim analogInSim;
     private boolean isInverted;
     private double voltageToDegrees = 360 / 5;
     private double m_offset;
+    private int polarity;
 
+    /**
+     * Construct and absolute encoder, most likely a US Digital MA3 encoder.
+     * 
+     * @param channel  analog in (sometimes also refered to as AIO) port on the
+     *                 roboRIO
+     * @param inverted set this to <i>TRUE</i> if physically turning the wheel
+     *                 <i>CLOCKWISE</i> (looking down on it from the top of the bot)
+     *                 <i>INCREASES</i> the voltage it returns
+     * @param offset   swerve offset (in <i>DEGREES</i>), like we've been using for
+     *                 the past three years. This value is <i>SUBTRACTED</i> from
+     *                 the output.
+     */
     public AbsoluteEncoder(int channel, boolean inverted, double offset) {
         analogIn = new AnalogInput(channel);
+        analogInSim = new AnalogInputSim(analogIn);
+        analogInSim.setVoltage(0);
         isInverted = inverted;
         m_offset = offset;
+        polarity = isInverted ? -1 : 1;
     }
 
+    /**
+     * Does some fancy mafs (mathematics) to figure out the value that the simulated
+     * absolute encoder should be at. Read comments in source code below.
+     * 
+     * @param turnVoltage voltage that will go to the turning motor, range: [-1, 1]
+     */
     public void sendVoltage(double turnVoltage) {
 
         // gear ratio between motor and wheel / encoder
-        double ratio = 12.8;
+        double gearRatio = 12.8;
 
         // maximum RPM for motor under 0 load
         double motorRPM = 5500;
 
         // how long it takes to run one loop of the periodic (in seconds)
         double tickPeriod = Robot.kDefaultPeriod;
+
         if (turnVoltage > 1) {
             turnVoltage = 1;
         } else if (turnVoltage < -1) {
@@ -42,24 +68,44 @@ public class AbsoluteEncoder {
 
         // started with motorRPM and converted to wheel rotations per tick
         // RPM * (min / sec) * (s / tick) * (wheel rotations / motor rotations)
-        double wheelRotationsPerTick = motorRPM / 60 * tickPeriod / ratio; // 0.143
+        double wheelRotationsPerTick = motorRPM / 60 * tickPeriod / gearRatio; // 0.143
         double wheelRotationsSinceLastTick = wheelRotationsPerTick * turnVoltage;
         double voltsSinceLastTick = 5 * wheelRotationsSinceLastTick;
-        double output = analogIn.getVoltage() + voltsSinceLastTick;
+        voltsSinceLastTick *= polarity;
+
+        double outputVoltage = analogIn.getVoltage() + voltsSinceLastTick;
+
+        SmartDashboard.putNumber("turning voltage" + analogIn.getChannel(), turnVoltage);
 
         // convert output to a number between 0 and 5
-        output = (((output % 5) + 5) % 5);
+        outputVoltage = (((outputVoltage % 5) + 5) % 5);
+
+        // basically this "hijacks" the simulated absolute encoder to say that it's
+        // reading the voltage that u give it, range: [0, 5]
+        analogInSim.setVoltage(outputVoltage);
+
     }
 
+    /**
+     * 
+     * @return the position of the wheel, in degrees. zero points toward the front
+     *         of the bot. the value increases as the swerve wheel is turned
+     *         clockwise.
+     */
     public double getDegrees() {
         if (isInverted) {
-            return (5 - analogIn.getVoltage() - m_offset) * voltageToDegrees;
+            return (5 - analogIn.getVoltage() ) * voltageToDegrees - m_offset;
         }
 
-        return (analogIn.getVoltage() - m_offset) * voltageToDegrees;
+        return (analogIn.getVoltage() ) * voltageToDegrees - m_offset;
 
     }
 
+    /**
+     * Simply calls the <code>{@code getDegrees()}</code> method for this object.
+     * 
+     * @return <code>{@code this.getDegrees()}</code> but in radians.
+     */
     public double getRadians() {
         return Math.toRadians(getDegrees());
     }
