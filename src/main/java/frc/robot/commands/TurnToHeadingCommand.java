2cf7237576ec0d031da7807b4c1d92cc4dd4b0c0
TiaSinghania <59597284+TiaSinghania@users.noreply.github.com>
2021-04-23 15:46:48 -0700

    Auton Move Commands (#13)
    * implemented basic move commands
    
    * fixed base behavior of move commands (challenges)
    
    * fixed move commands
    
    * Javadocs to FieldRelativeCommand
    
    * GTP Description
    
    * Javadocs to MDC command
    
    Co-authored-by: noam987 <noam@arie11.com>
    Co-authored-by: Ethan Shen <45775339+Skipppyyy@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/TurnToHeadingCommand.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+
+/* ideas for default to current pos
+    1. use boolean flags when "with" commands are called combined with if statements in initialize
+         (might use two arrays to keep things neat instead of having like 6 variables)
+    2. could we use the thing where you passed in methods to use as values so they could be updated
+        so like instead of initially declaring them and setting them as the current VALUE
+        we could declare them and set them to be the most updated result of *this method*
+    3. how would we feel about gotoposition having a getcurrentpos method and a gettargetpos method
+        in gotoposition the targetpos is by default the currentpos
+        but child methods can override if necessary
+*/
+package frc.robot.commands;
+
+import frc.robot.subsystems.SwerveDrivetrain;
+
+/**
+* Turns robot to field relative heading
+*/
+public class TurnToHeadingCommand extends GoToPositionCommand {
+    /**
+     * Constructs the command.
+     * 
+     * @param drivetrain required subsystem
+     */
+    public TurnToHeadingCommand(SwerveDrivetrain drivetrain) {
+        super(drivetrain);
+    }
+
+    /**
+     * Sets the target rotation.
+     * 
+     * @param targetHeading the desired heading (a value in radians)
+     * @return returning the object allows for method chaining.
+     */
+    public TurnToHeadingCommand withHeading (double targetHeading){
+        m_targetRotation = targetHeading;
+        return this;
+    }
+    
+
+}
