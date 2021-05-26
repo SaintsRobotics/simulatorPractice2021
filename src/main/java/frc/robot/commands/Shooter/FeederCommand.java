7dbfcbdf95377341d4e84e6f4beb7e9f838ac5ee
LennyAbbas <lennyabbas3@gmail.com>
2021-05-26 16:22:38 -0700

    made 2 new commands for feeder and shooter

--- src/main/java/frc/robot/commands/Shooter/FeederCommand.java
+++ src/main/java/frc/robot/commands/Shooter/FeederCommand.java
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.Shooter;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class ShootCommand extends CommandBase {
-  /** Creates a new ShootCommand. */
-  public ShootCommand() {
+public class FeederCommand extends CommandBase {
+  /** Creates a new FeederCommand. */
+  public FeederCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
