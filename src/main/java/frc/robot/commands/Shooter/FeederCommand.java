803832f99f5ebffc9e66bac30a2667ff7afd1539
LennyAbbas <lennyabbas3@gmail.com>
2021-05-26 16:33:07 -0700

    finished FeederCommand and ShooterCommand

--- src/main/java/frc/robot/commands/Shooter/FeederCommand.java
+++ src/main/java/frc/robot/commands/Shooter/FeederCommand.java
@@ -5,26 +5,31 @@
 package frc.robot.commands.Shooter;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class FeederCommand extends CommandBase {
+  private ShooterSubsystem m_shooter;
+
   /** Creates a new FeederCommand. */
-  public FeederCommand() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  public FeederCommand(ShooterSubsystem shooter) {
+    m_shooter = shooter;
+    addRequirements(m_shooter);
   }
 
-  // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_shooter.turnOnFeeder();
+  }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
-  // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_shooter.turnOffFeeder();
+  }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
