d19688b28cb8712fc8c49dfecfbf67bce41daf94
LennyAbbas <lennyabbas3@gmail.com>
2021-05-26 16:15:57 -0700

    logic to control feeder in shooter subsystem

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -16,22 +17,32 @@ public class ShooterSubsystem extends SubsystemBase {
   private CANSparkMax m_leftShooter;
   private CANSparkMax m_rightShooter;
   private SpeedControllerGroup m_shooter;
-
   private boolean m_isShooterOn;
+
+  private WPI_VictorSPX m_kicker;
+  private WPI_VictorSPX m_wheels;
+  private SpeedControllerGroup m_feeder;
+  private boolean m_isFeederOn;
 
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem() {
     m_leftShooter = new CANSparkMax(Constants.Shooter.LEFT_SHOOTER_PORT, MotorType.kBrushless);
     m_rightShooter = new CANSparkMax(Constants.Shooter.LEFT_SHOOTER_PORT, MotorType.kBrushless);
     m_leftShooter.setInverted(true);
-
     m_shooter = new SpeedControllerGroup(m_leftShooter, m_rightShooter);
     m_isShooterOn = false;
+
+    m_kicker = new WPI_VictorSPX(Constants.Shooter.KICKER_PORT);
+    m_wheels = new WPI_VictorSPX(Constants.Shooter.WHEELS_PORT);
+    m_wheels.setInverted(true);
+    m_feeder = new SpeedControllerGroup(m_kicker, m_wheels);
+    m_isFeederOn = false;
   }
 
   @Override
   public void periodic() {
     m_shooter.set(m_isShooterOn ? Constants.Shooter.SHOOTER_SPEED : 0);
+    m_feeder.set(m_isFeederOn ? Constants.Shooter.FEEDER_SPEED : 0);
 
     SmartDashboard.putBoolean("Is Shooter On", m_isShooterOn);
     SmartDashboard.putNumber("Shooter Speed", m_shooter.get());
@@ -43,5 +54,13 @@ public class ShooterSubsystem extends SubsystemBase {
 
   public void turnOffShooter() {
     m_isShooterOn = false;
+  }
+
+  public void turnOnFeeder() {
+    m_isFeederOn = true;
+  }
+
+  public void turnOffFeeder() {
+    m_isFeederOn = false;
   }
 }
