ba0083355112dedd2111c0842edaddbd6f291e80
hp-programming <controlsystems@saintsrobotics.com>
2021-06-14 16:16:42 -0700

    Fix swerve x direction

--- src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
@@ -99,8 +99,8 @@ public class SwerveDrivetrain extends SubsystemBase {
                                 MotorType.kBrushless);
                 m_backRightTurningMotor.setIdleMode(IdleMode.kCoast);
 
-                m_frontLeftDriveMotor.setInverted(true);
-                m_backLeftDriveMotor.setInverted(true);
+                m_frontRightDriveMotor.setInverted(true);
+                m_backRightDriveMotor.setInverted(true);
 
                 m_frontLeftTurningEncoder = new AbsoluteEncoder(SwervePorts.FRONT_LEFT_TURNING_ENCODER_PORT, true,
                                 SwerveConstants.FRONT_LEFT_ROTATION_OFFSET);
